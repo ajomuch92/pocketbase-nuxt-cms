333a1263cae4ec494c4e5736198982f54d707cc8
ajomuch92 <aaronmontes_92@yahoo.es>
2025-12-14 11:41:42 -0600

    feat: add full and main layout components

---
+++ app/layouts/fullLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex items-center justify-center min-h-screen bg-muted relative">
+    <RouterLink
+      v-if="showGoHome"
+      to="/"
+      class="absolute top-4 left-4"
+    >
+      <AppLogo class="w-auto h-6 shrink-0" />
+    </RouterLink>
+    <NuxtPage />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const { showGoHome = true } = defineProps<{
+  showGoHome?: boolean
+}>()
+</script>
---
+++ app/layouts/mainLayout.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <UHeader>
+      <template #left>
+        <NuxtLink to="/">
+          <AppLogo class="w-auto h-6 shrink-0" />
+        </NuxtLink>
+
+        <TemplateMenu />
+      </template>
+
+      <template #right>
+        <UColorModeButton />
+
+        <NuxtLink to="/login">
+          <UButton
+            class="cursor-pointer"
+            aria-label="Login"
+            icon="i-lucide-log-in"
+            color="primary"
+            variant="ghost"
+          >
+            Login
+          </UButton>
+        </NuxtLink>
+      </template>
+    </UHeader>
+
+    <UMain>
+      <NuxtPage />
+    </UMain>
+
+    <USeparator icon="i-simple-icons-nuxtdotjs" />
+
+    <UFooter>
+      <template #left>
+        <p class="text-sm text-muted">
+          Pocketbase Nuxt CMS• {{ new Date().getFullYear() }}
+        </p>
+      </template>
+    </UFooter>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
