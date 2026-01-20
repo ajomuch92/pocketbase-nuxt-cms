dbc2d748fc94420013c7ed45f130d34004958535
ajomuch92 <aaronmontes_92@yahoo.es>
2026-01-19 20:42:53 -0600

    feat: add dashboard layout and page components for improved UI structure

---
+++ app/layouts/dashboardLayout.vue
@@ -0,0 +1,120 @@
+<template>
+  <UDashboardGroup>
+    <UDashboardSidebar
+      collapsible
+      resizable
+      :ui="{ footer: 'border-t border-default' }"
+    >
+      <template #header="{ collapsed }">
+        <AppLogo
+          v-if="!collapsed"
+          class="h-5 w-auto shrink-0"
+        />
+        <UIcon
+          v-else
+          name="i-simple-icons-nuxtdotjs"
+          class="size-5 text-primary mx-auto"
+        />
+      </template>
+
+      <template #default="{ collapsed }">
+        <UButton
+          :label="collapsed ? undefined : 'Search...'"
+          icon="i-lucide-search"
+          color="neutral"
+          variant="outline"
+          block
+          :square="collapsed"
+        >
+          <template
+            v-if="!collapsed"
+            #trailing
+          >
+            <div class="flex items-center gap-0.5 ms-auto">
+              <UKbd
+                value="meta"
+                variant="subtle"
+              />
+              <UKbd
+                value="K"
+                variant="subtle"
+              />
+            </div>
+          </template>
+        </UButton>
+
+        <UNavigationMenu
+          :collapsed="collapsed"
+          :items="items[0]"
+          orientation="vertical"
+        />
+
+        <!-- <UNavigationMenu
+          :collapsed="collapsed"
+          :items="items[1]"
+          orientation="vertical"
+          class="mt-auto"
+        /> -->
+      </template>
+
+      <template #footer="{ collapsed }">
+        <UButton
+          :avatar="{
+            src: 'https://github.com/benjamincanac.png'
+          }"
+          :label="collapsed ? undefined : 'Benjamin'"
+          color="neutral"
+          variant="ghost"
+          class="w-full"
+          :block="collapsed"
+        />
+      </template>
+    </UDashboardSidebar>
+    <UMain class="px-8 py-10">
+      <NuxtPage />
+    </UMain>
+  </UDashboardGroup>
+</template>
+
+<script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+const items: NavigationMenuItem[][] = [[{
+  label: 'Home',
+  icon: 'i-lucide-house',
+  active: true,
+  to: '/dashboard'
+}, {
+  label: 'Categories',
+  icon: 'i-lucide-inbox'
+}, {
+  label: 'Tags',
+  icon: 'i-lucide-tag'
+}, {
+  label: 'Posts',
+  icon: 'i-lucide-file-text'
+}, {
+  label: 'Comments',
+  icon: 'i-lucide-message-square'
+}, {
+  label: 'Media',
+  icon: 'i-lucide-image'
+}, {
+  label: 'Pages',
+  icon: 'i-lucide-file'
+}, {
+  label: 'Users',
+  icon: 'i-lucide-users'
+}, {
+  label: 'Settings',
+  icon: 'i-lucide-settings',
+  defaultOpen: true,
+  children: [{
+    label: 'General'
+  }, {
+    label: 'Members'
+  }, {
+    label: 'Notifications'
+  }]
+}]]
+</script>
---
+++ app/pages/dashboard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Dashboard</div>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  name: 'Dashboard',
+  layout: 'dashboard-layout'
+})
+
+useSeoMeta({
+  title: 'Dashboard - Pocketbase Nuxt CMS Starter Template'
+})
+</script>
